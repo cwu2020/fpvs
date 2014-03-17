763a60ee66acb956024bf71b54cdd676fe5f6fbd
dagobert renouf <dagobert.king@gmail.com>
2014-03-17 09:33:14 +0100

    Added basis of switch panel function

--- js/fpvs.js
+++ js/fpvs.js
@@ -1,1 +1,29 @@
 /* Full Page vertical Slideshow javascript */
+$(window).scroll(function(){
+  
+});
+
+$(document).ready(function(){
+
+	switchPanel();
+
+})
+
+var currentFpvs = 1;
+
+function switchPanel(){
+  
+  console.log(currentFpvs);
+  
+  if(currentFpvs < 100){
+  
+  	$(".fpvs-slide:nth-child(" + currentFpvs + ")").hide(1000, function(){
+
+  		currentFpvs++;
+  		console.log(currentFpvs);
+
+  	});
+
+  }
+
+}
